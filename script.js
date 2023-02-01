91cd09663acc355155ab1688951a52e1cfe88417
mrunali-rode <mrunalirode2211@gmail.com>
2023-02-01 14:17:19 +0530

    modified

---
+++ script.js
@@ -0,0 +1,21 @@
+// function Person() {
+//     this.name = 'Nisha'
+// }
+
+// // adding property 
+// Person.prototype.name = 'Peter';
+// Person.prototype.age = 22
+
+// const person1 = new Person();
+
+// console.log(person1.name);
+// console.log(person1.age); 
+
+let obj =  {
+    property: "1500 sqft",
+    villa:"ADA Colony Naini",
+    address:'Prayagaj Uttar Pradesh',
+    Dist:'Prayagraj'
+};
+let result=Object.keys(obj)
+console.log(result)
